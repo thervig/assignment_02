e12b1f4fca8b25cec2c770d5eba119faaecb416e
thervig <thervig@asu.edu>
2016-01-31 15:38:34 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,17 +9,17 @@ class TestAssignmentTwo_None(unittest.TestCase):
         """
         Think about what 'is' does here.
         """
-        truth = ____
+        truth = None
         self.assertEqual(truth, None is None)
 
     def test_none_is_unique(self):
         """
         This is a great place to launch an interpreter and check
-         what the tight hand side of the equalities evaluate to.
+         what the right hand side of the equalities evaluate to.
         """
-        truth = ____
+        truth = None
         self.assertEqual(truth, None is not 0)
-        truth = ____  # This resets the value of truth
+        truth = 0 # This resets the value of truth
         self.assertEqual(truth, None is not False)
 
     def test_none_existence(self):
@@ -44,7 +44,6 @@ class TestAssignmentTwo_None(unittest.TestCase):
 
         """
         The take away here is that truthiness is not the same as None
-
         None evaluates to False in these if statements, as does an
         empty list, but None != (is not equal to) False.
         """
@@ -66,16 +65,16 @@ class TestAssignmentTwo_Strings(unittest.TestCase):
         """
 
         string = 'Hello World'
-        self.assertEqual(____, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
         string = "Hello World"
-        self.assertEqual(____, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
         string = '''Hello World'''
-        self.assertEqual(____, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
         string = """Hello World"""
-        self.assertEqual(____, isinstance(string, str))
+        self.assertEqual(True, isinstance(string, str))
 
     def test_escaping_quotes(self):
         """
@@ -87,7 +86,7 @@ class TestAssignmentTwo_Strings(unittest.TestCase):
         a = "He said, \"Don't forget to relax a little\""
         b = 'He said, "Don\'t" forget to relax a little'
 
-        self.assertEqual(____, a == b)
+        self.assertEqual(True, a == b)
 
     def test_string_concatenation(self):
         """
@@ -95,26 +94,24 @@ class TestAssignmentTwo_Strings(unittest.TestCase):
         See the Zen of Python for which is preferable
         """
         string = "Go" + " Sun Devils"
-        self.assertEqual(____, string)
+        self.assertEqual(True, string)
 
         string = "Go" " Sun Devils"
-        self.assertEqual(____, string)
+        self.assertEqual(True, string)
 
     def test_in(self):
         """
         The in operator can be helpful in finding
         substrings.
-
         Once the complexity gets high, I would suggest switching
         to the 're' module (for Regex).
-
         For these, fix the assertion methods
         """
         cities = '"New York", "Boston", "Chicago", "Dallas", "St. Louis", "Phoenix" '
-        self.assert____('Dallas' in cities)
-        self.assert____('"Dallas"' in cities)
-        self.assert____('ton' in cities)
-        self.assert____("\", \"" in cities)
+        self.assertEqual('Dallas' in cities)
+        self.assertEqual('"Dallas"' in cities)
+        self.assertEqual('ton' in cities)
+        self.assertEqual("\", \"" in cities)
 
     def test_format(self):
         """
@@ -124,11 +121,11 @@ class TestAssignmentTwo_Strings(unittest.TestCase):
         """
 
         s = 'The current index is {}'
-        self.assertEqual(____,isinstance(s, str))
+        self.assertEqual(True,isinstance(s, str))
 
         i = 0
 
-        truth = 'The current index is _____'  # Replace the ____
+        truth = 'The current index is {}'  # Replace the ____
         self.assertEqual(truth, s.format(i))
 
     def test_string_cases(self):
@@ -136,16 +133,15 @@ class TestAssignmentTwo_Strings(unittest.TestCase):
         Another common string manipulation set borrowed from
         Python Koans.
         """
-        self.assertEqual(__, 'guido'.capitalize())
-        self.assertEqual(__, 'guido'.upper())
-        self.assertEqual(__, 'TimBot'.lower())
-        self.assertEqual(__, 'guido van rossum'.title())
-        self.assertEqual(__, 'ToTaLlY aWeSoMe'.swapcase())
+        self.assertEqual(True, 'guido'.capitalize())
+        self.assertEqual(True, 'guido'.upper())
+        self.assertEqual(True, 'TimBot'.lower())
+        self.assertEqual(True, 'guido van rossum'.title())
+        self.assertEqual(True, 'ToTaLlY aWeSoMe'.swapcase())
 
     def test_format_rounding(self):
         """
         This test mirrors real world code that I use frequently.
-
         Assume that you have a bunch of data in a cvs file.  You
         have read in the data and now want to print some of it
         to screen or pipe it into another file.  The only thing is,
@@ -156,28 +152,26 @@ class TestAssignmentTwo_Strings(unittest.TestCase):
         s = '{}'
 
         # Check to see what the heck 's' is
-        self.assertEqual(____, isinstance(s, str))
+        self.assertEqual(True, isinstance(s, str))
 
-        rounded_pi = _________
+        rounded_pi = 3.14
         self.assertEqual(rounded_pi, s.format(round(pi, 4)))
 
     def test_translation(self):
         """
         Take a look at the string module to see how this is working.
         The example is from PyMoTW (see the syllabus for a link).
-
         In Python 3 (the version we are using) `maketrans` has moved
         from the string module to a method on the `str` type.  This
         is why we do not need to `import string`
-
         Fill in the truth value
         """
 
         leet = str.maketrans('abegiloprstz', '463611092572')
         s = 'The quick brown fox jumped over the lazy dog.'
         s.translate(leet)  # Translate the string here
-        
-        truth = ____  # Truth is the newly translated string 
+
+        truth = '7h3 qu1ck 620wn f0x jum93d 0v32 7h3 142y d06'  # Truth is the newly translated string
 
         self.assertEqual(truth, s)
 
@@ -186,7 +180,8 @@ class TestAssignmentTwo_Strings(unittest.TestCase):
         Write your own test here demonstrating either string
         or None usage that has not been demonstrated above.
         """
-        self.assertTrue(False)  # You can either fix this line or remove it once the test is in.
+        NewString = 'xyz'
+        self.assertContains(True, NewString)  # You can either fix this line or remove it once the test is in.
 
     def tearDown(self):
         pass
